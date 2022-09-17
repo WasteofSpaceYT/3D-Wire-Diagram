714835f27e02b26e1826731be3e38bbe2b2cef5a
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-09-17 17:30:02 -0400

    Added Home Page

---
+++ diagram/src/routes/Navbar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<div style="height: 50px">
+    <h1>&#123;D&#125;</h1>
+    <Button>E</Button>
+</div>
--- diagram/src/routes/home.svelte
+++ diagram/src/routes/home.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-
+import { onMount } from "svelte";
+    let width = 10;
+    let height = 5;
+    onMount(() => {
+        let room = document.getElementById("room");
+        room.style.width = `${width*30}px`;
+        room.style.height = `${height*30}px`;
+    })
 </script>
-<div>
-    <h1>Home</h1>
+<div style="margin:20px">
+    Height: <input type="number" bind:value={width} on:change={() => {
+        document.getElementById("room").style.width = `${width}px`;
+    }} />ft
+    <br />
+    Width: <input type="number" bind:value={height} on:change={() => {
+        document.getElementById("room").style.height = `${height}px`;
+    }} />ft
+    <div id="room" style="border: 1px solid black; margin: 25px">
+    </div>
+    <button href={`/diagram?width=${width}&height=${height}`}>Get to Wirin'</button>
 </div>
