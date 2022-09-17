387bbe55729e87fd0174d65e26ab6ca2e15f6ea1
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-09-17 17:55:11 -0400

    Now its done

--- diagram/src/routes/home.svelte
+++ diagram/src/routes/home.svelte
@@ -9,14 +9,14 @@ import { onMount } from "svelte";
     })
 </script>
 <div style="margin:20px">
-    Height: <input type="number" bind:value={width} on:change={() => {
+    Width: <input type="number" bind:value={width} on:change={() => {
         document.getElementById("room").style.width = `${width}px`;
     }} />ft
     <br />
-    Width: <input type="number" bind:value={height} on:change={() => {
+    Height: <input type="number" bind:value={height} on:change={() => {
         document.getElementById("room").style.height = `${height}px`;
     }} />ft
     <div id="room" style="border: 1px solid black; margin: 25px">
     </div>
-    <button href={`/diagram?width=${width}&height=${height}`}>Get to Wirin'</button>
+    <a href={`/diagram?width=${width}&height=${height}`}><button>Get to Wirin'</button></a>
 </div>
