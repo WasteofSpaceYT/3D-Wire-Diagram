3225025ac7483f2a76ddda668ddb4f9d51f0656e
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-09-17 18:56:49 -0400

    I dislike the lack of being able to test routes in dev

--- diagram/src/App.svelte
+++ diagram/src/App.svelte
@@ -3,9 +3,11 @@ import { Router, Link, Route } from "svelte-routing";
 import Diagram from "./routes/diagram.svelte";
 import Home from "./routes/home.svelte";
 let isSubmitted = false;
+let url = "";
 </script>
 <div>
-	<Router>
+	<Router url={url}>
+		<h1>{url}</h1>
 		<Route path="/diagram" component={Diagram} />
 		<Route path="/">
 	<Home />
