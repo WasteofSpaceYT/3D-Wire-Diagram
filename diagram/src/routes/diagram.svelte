4d0bf56a5c8fc123e83f0b02f1bec9d11e4bd32c
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-09-18 17:31:49 -0400

    Update diagram.svelte

--- diagram/src/routes/diagram.svelte
+++ diagram/src/routes/diagram.svelte
@@ -98,7 +98,7 @@
 			renderer.setAnimationLoop(render);
 			for(let i = 0; i < receptacles.scene.children.length; i++){
 				//@ts-ignore
-				receptacles.scene.children[i].material.color = new THREE.Color(0x000000);
+				console.log(receptacles.scene.children[i])
 			}
 			scene.add(receptacles.scene)
 		},
