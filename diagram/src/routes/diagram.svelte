d8f1b0ee58063c631af881e01d52ba42c6397226
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2022-09-19 10:33:21 -0400

    Update diagram.svelte

--- diagram/src/routes/diagram.svelte
+++ diagram/src/routes/diagram.svelte
@@ -68,6 +68,7 @@
 
 			for (let i = 0; i < intersections.length; i++) {
 				let point = intersections[i].point;
+				console.log(point)
 				cube.position.set(point)
 				console.log(cube.position)
 			}
